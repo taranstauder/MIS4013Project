3461f563813ab5c19e669090e15b30c46131becd
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-11-21 14:12:16 -0600

    Add files via upload

---
+++ movie-add.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Movie</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Add Movie</h1>
+    <form method="post" action="movie-add-save.php">
+        <div>
+            <label for="movieName" class="form-label">Name</label>
+            <input type="text" name="mName" id="movieName" class="form-control" aria-describedby="nameHelp">
+            <div id="nameHelp" class="form-text" >Enter the movie's name</div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
