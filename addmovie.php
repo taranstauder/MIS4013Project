5d77919f1895c91f0fc313acabc668060c31ea88
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-21 14:24:05 -0600

    Update addmovie.php

--- addmovie.php
+++ addmovie.php
@@ -83,7 +83,7 @@ $conn->close();
                 <div class="mb-3">
                   <label for="moviename" class="form-label">Name</label>
                   <input type="text" class="form-control" id="moviename" aria-describedby="nameHelp" name="iName">
-                  <div id="nameHelp" class="form-text">Enter the customer's name.</div>
+                  <div id="nameHelp" class="form-text">Enter the movie's name.</div>
                 </div>
                 <input type="hidden" name="saveType" value="Add">
                 <button type="submit" class="btn btn-primary">Submit</button>
