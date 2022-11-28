97d4d66eef6d14b4ce8f304f054b23a08f51c845
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-11-28 14:39:56 -0600

    Update testHome.php

--- testHome.php
+++ testHome.php
@@ -6,66 +6,6 @@
   href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
 />
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-      <style>
-      html,
-      body {
-        position: relative;
-        height: 100%;
-      }
-
-        body {
-        background: #eee;
-        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-        font-size: 14px;
-        color: #000;
-        margin: 0;
-        padding: 0;
-      }
-
-      .swiper {
-        width: 100%;
-        height: 100%;
-      }
-
-      .swiper-slide {
-        text-align: center;
-        font-size: 18px;
-        background: #fff;
-
-        /* Center slide text vertically */
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-        -webkit-justify-content: center;
-        justify-content: center;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        -webkit-align-items: center;
-        align-items: center;
-      }
-
-      .swiper-slide img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
-
-      .swiper-slide {
-        width: 60%;
-      }
-
-      .swiper-slide:nth-child(2n) {
-        width: 40%;
-      }
-
-      .swiper-slide:nth-child(3n) {
-        width: 20%;
-      }
-    </style>
 </head>
 <body>
 <br>
