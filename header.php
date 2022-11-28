06d653f99412626a38f5179684697a7a686f9d9b
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-11-28 13:54:23 -0600

    Update header.php

--- header.php
+++ header.php
@@ -4,4 +4,22 @@
     <div style="background-color: black; font-family: helvetica; text-align: center; color: white;" class="jumbotron">
 	    <h1>Movies Я Us</h1>
     </div>
+<nav class="navbar navbar-expand-lg bg-dark">
+<div class="container-fluid" style="font-family: helvetica;">
+	<ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="index.php">Customers</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="review.php">Reviews</a>
+	  </li>
+	<li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="addmovie.php">Add Movies</a>
+	  </li>
+	<li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="suppliers.php">Suppliers</a>
+	  </li>
+	</ul>
+</div>
+</nav>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
