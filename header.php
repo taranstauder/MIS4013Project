e492a8709a9bb9556d49288b8c7cb2b91fe08835
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-11-28 13:33:47 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,8 @@
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Final Project </title>
+    <div style="background-color: black; font-family: monospace;" class="jumbotron">
+	    <h1>E-Commerce 4013: Final Project</h1>
+        <h2>Movies R Us</h2>
+    </div>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
