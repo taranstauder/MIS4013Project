79a94799bb5451c50bf190e4ba250bcf67b023c9
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-28 15:57:21 -0600

    Update reviews.php

--- reviews.php
+++ reviews.php
@@ -105,7 +105,7 @@ if ($result->num_rows > 0) {
             </td>
             <td>
              <form method="post" action="">
-                <input type="hidden" name="iid" value="<?=$row["title"]?>" />
+                <input type="hidden" name="iid" value="<?=$row["rtitle"]?>" />
                 <input type="hidden" name="saveType" value="Delete">
                 <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
               </form>
