323d1fa480e786a670929a3b58b690d7367a4819
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-01 14:19:03 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,11 +2,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <style>
 .footer {
-  position: inherit;
+  position: absolute;
   left: 0;
   bottom: 0;
   width: 100%;
-  font-family: Helvetica;
   text-align: center;
 }
 </style>
