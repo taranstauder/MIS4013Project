ba7143e969d67e1c39e4d048467f84a3ab69ede4
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-01 12:46:43 -0600

    Update glide.php

--- glide.php
+++ glide.php
@@ -16,9 +16,9 @@
 <div class="glide">
   <div class="glide__track" data-glide-el="track">
     <ul class="glide__slides">
-      <li class="glide__slide">0</li>
-      <li class="glide__slide">1</li>
-      <li class="glide__slide">2</li>
+      <li class="glide__slide"><img src="https://static.toiimg.com/photo/msid-5348868/5348868.jpg?26276"></li>
+      <li class="glide__slide"><img src="https://static.toiimg.com/photo/msid-5348868/5348868.jpg?26276"></li>
+      <li class="glide__slide"><img src="https://static.toiimg.com/photo/msid-5348868/5348868.jpg?26276"></li>
     </ul>
   </div>
     <div class="glide__arrows" data-glide-el="controls">
