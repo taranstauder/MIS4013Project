08a4975b464f99c5ffbf7379c086f07525b66cf9
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-01 11:32:37 -0600

    Update testHome.php

--- testHome.php
+++ testHome.php
@@ -15,13 +15,13 @@
       <div class="swiper-wrapper">
         <div class="swiper-slide"><img src="https://static.toiimg.com/photo/msid-5348868/5348868.jpg?26276"></div>
         <div class="swiper-slide"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/7/7f/Smile_%282022_film%29.jpg/220px-Smile_%282022_film%29.jpg"></div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
+        <div class="swiper-slide"><img src="https://m.media-amazon.com/images/M/MV5BMjNiMDA0OWQtZGY1MC00N2EwLTkxNDYtYmVkNTk1NzJiZjFmXkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg"></div>
+        <div class="swiper-slide"><img src="https://static.wikia.nocookie.net/jurassicpark/images/c/ce/JP-MoviePoster.jpg/revision/latest?cb=20220215213506"></div>
+        <div class="swiper-slide"><img src="https://static.wikia.nocookie.net/starwars/images/5/53/SWTLJExpandedEdition.jpg/revision/latest?cb=20180212223228"></div>
+        <div class="swiper-slide"><img src="https://c8.alamy.com/comp/PXNB80/titanic-original-movie-poster-PXNB80.jpg"></div>
+        <div class="swiper-slide"><img src="https://c8.alamy.com/comp/E5MCMW/elf-will-ferrell-2003-c-new-linecourtesy-everett-collection-E5MCMW.jpg"></div>
+        <div class="swiper-slide"><img src="https://upload.wikimedia.org/wikipedia/en/8/8c/Indiana_Jones_and_the_Last_Crusade.png"></div>
+        <div class="swiper-slide"><img src="https://m.media-amazon.com/images/M/MV5BYzI0N2M4ZDItY2VmOS00OGVhLWI2NmUtOGU0NWM3MGZkM2Y3XkEyXkFqcGdeQXVyMDc2NTEzMw@@._V1_.jpg"></div>
       </div>
       <div class="swiper-button-next"></div>
       <div class="swiper-button-prev"></div>
@@ -43,7 +43,6 @@
         slidesPerGroup: 3,
         loop: true,
         loopFillGroupWithBlank: true,
-        effect: "fade",
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
