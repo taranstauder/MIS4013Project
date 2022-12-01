4781bc44766e1ac8f394f77e67866c8a286ab673
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-01 14:51:55 -0600

    Update header.php

--- header.php
+++ header.php
@@ -50,6 +50,11 @@ body {
 	  </li>
 </div>
 </nav>
+<style>
+  .container-fluid{
+	  position: fixed;
+	}
+ </style>
 <br>
     </div>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
