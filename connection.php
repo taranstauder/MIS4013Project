95512a3db53fed9139d4da959dd9edaf86da0679
Dasha Davydova <dasha.davydova@ou.edu>
2022-12-02 10:55:10 -0600

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php
+
+$conn = "";
+
+try {
+	$servername = "159.89.47.44";
+$username = "davyddov_davy0000";
+$password = "mis4013project";
+$dbname = "davyddov_mis4013project";
+
+	$conn = new PDO(
+		"mysql:host=$servername; dbname=davyddov_mis4013project",
+		$username, $password
+	);
+	
+$conn->setAttribute(PDO::ATTR_ERRMODE,
+					PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
+
+?>
