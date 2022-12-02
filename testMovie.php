7cb921177090b29441896047f837dacc2ed05232
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-01 19:02:31 -0600

    Create testMovie.php

---
+++ testMovie.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html style="background-color: black;">
+  <title>Movies</title>
+<head> <?php include("header.php");?></head>
+<body style="background-color: black;">
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Movie</th>
+      <th>Starring Actor</th>
+      <th>Director</th>
+      <th>Summary</th>
+      <th>Review Rating</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "159.89.47.44";
+$username = "davyddov_davy0000";
+$password = "mis4013project";
+$dbname = "davyddov_mis4013project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT m.title, a.name, d.dirname, m.summary, m.summary from Movie m join Cast c on m.movid=c.movid join Actor a on c.actid=a.actid join Director d on m.movid=d.movid join Review r on m.movid=r.movid";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["title"]?></td>
+    <td><?=$row["name"]?></td>
+    <td><?=$row["dirname"]?></td>
+    <td><?=$row["summary"]?></td>
+    <td><?=$row["rating"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <?php include("footer.php");?>
+</body>
+</html>
