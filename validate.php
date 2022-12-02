fd36dec006c797c931bdc346a83d0cc17ebc339c
Dasha Davydova <dasha.davydova@ou.edu>
2022-12-02 10:57:09 -0600

    Update validate.php

--- validate.php
+++ validate.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		
 		if(($user['username'] == $username) &&
 			($user['password'] == $password)) {
-				header("location: adminpage.php");
+				header("location: admoviecopy.php");
 		}
 		else {
 			echo "<script language='javascript'>";
