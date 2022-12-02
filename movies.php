37ab17d4308cd9f248b8b1f76c72a88318dc2620
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-01 19:12:59 -0600

    Update movies.php

--- movies.php
+++ movies.php
@@ -26,7 +26,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT m.title, a.name, d.dirname, m.summary from Movie m join Cast c on m.movid=c.movid join Actor a on c.actid=a.actid join Director d on m.movid=d.movid join Review r on m.movid=r.movid";
+$sql = "SELECT m.title, a.name, d.dirname, m.summary from Movie m join Cast c on m.movid=c.movid join Actor a on c.actid=a.actid join Director d on m.movid=d.movid";
+  // join Review r on m.movid=r.movid  
   //<th>Review Rating</th>
   //r.rating
   //<td><?=$row["rating"]
