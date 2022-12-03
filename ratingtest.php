5065f8875e883abfc86c1b1b01aaf7a3e74620e4
Dasha Davydova <dasha.davydova@ou.edu>
2022-12-02 21:12:05 -0600

    Update ratingtest.php

--- ratingtest.php
+++ ratingtest.php
@@ -44,7 +44,7 @@ $password = "mis4013project";
 $dbname = "davyddov_mis4013project";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
