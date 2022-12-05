60cd38b0bd8ccc0a477099e2dc65bc66256b5a60
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-05 15:15:31 -0600

    Update movies.php

--- movies.php
+++ movies.php
@@ -3,7 +3,7 @@
   <title>Movies</title>
 <head> <?php include("header.php");?></head>
 <body style="background-color: black; color:white;">
-<table class="table table-dark table-striped">
+<table class="table table-dark table-hover">
   <thead>
     <tr>
       <th>Movie</th>
