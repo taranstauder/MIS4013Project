cd1646a661a7fe25d5e8c210342f77002183b55e
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-05 16:52:34 -0600

    Update trailer.php

--- trailer.php
+++ trailer.php
@@ -43,7 +43,6 @@ $conn->close();
     </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <?php include("footer.php");?>
-<?php include("footer.php");?>
 </body>
 
 </html>
