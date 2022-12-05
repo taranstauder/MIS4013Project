fff2d7d42aacacbb47add5b67751e32290e7ae46
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-05 17:24:06 -0600

    Update displayTrailer.php

--- displayTrailer.php
+++ displayTrailer.php
@@ -5,17 +5,22 @@ switch ($mName) {
         echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/5PSNL1qE6VY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         break;
     case "Elf":
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/gW9wRNqQ_P8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         break;
     case "Harry Potter and the Sorcerer's Stone":
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/VyHV0BRtdxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         break;
     case "Smile":
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/BcDK7lkzzsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         break;
     case "Titanic":
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/CHekzSiZjrY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         break;
     case "Top Gun: Maverick":
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/giXco2jaZ_4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         break;
     case "Wall E":
-        echo "ur watching wall e";
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/CZ1CATNbXg0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         break;
 }
 ?>
