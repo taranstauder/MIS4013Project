9a972ede04ffc99c5ed48fbf42fe2c5ca4acece4
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-05 17:31:22 -0600

    Update displayTrailer.php

--- displayTrailer.php
+++ displayTrailer.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Trailer</title>
+</head>
+<body style="background-color: black; color:white;">
+<?php include("header.php");?>
 <?php
 $mName = $_GET["movieTrailer"];
 switch ($mName) {
@@ -24,3 +34,55 @@ switch ($mName) {
         break;
 }
 ?>
+<table class="table" style="color:white;">
+  <thead style="align-content: left; white-space: nowrap;">
+    <tr>
+      <th>Movie</th>
+      <th>Star Actor</th>
+      <th>Director</th>
+      <th>Summary</th>
+      <th>Average Rating</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "159.89.47.44";
+$username = "davyddov_davy0000";
+$password = "mis4013project";
+$dbname = "davyddov_mis4013project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT m.title, a.name, d.dirname, m.summary, ROUND(AVG(r.rating),1) as rating from Movie m join Cast c on m.movid=c.movid join Actor a on c.actid=a.actid join Director d on m.movid=d.movid join Review r on m.movid=r.movid group by m.title, a.name, d.dirname, m.summary"; 
+  
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    if($row["title"] == $mName)
+?>
+  <tr>
+    <td><?=$row["title"]?></td>
+    <td style="white-space: nowrap;"><?=$row["name"]?></td>
+    <td style="white-space: nowrap;"><?=$row["dirname"]?></td>
+    <td><?=$row["summary"]?></td>
+    <td style="text-align:center;"><?=$row["rating"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <?php include("footer.php");?>
+</body>
+</html>
