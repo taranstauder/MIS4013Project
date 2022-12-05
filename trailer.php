9a55c6a1fbaf887828d1fe9c570ea92baf81fbe8
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-05 16:28:48 -0600

    Update trailer.php

--- trailer.php
+++ trailer.php
@@ -39,6 +39,7 @@ if ($result->num_rows > 0) {
 $conn->close();
 ?>
                   </select>
+      <input type="submit" value="Show the the trailer!">
     </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <?php include("footer.php");?>
