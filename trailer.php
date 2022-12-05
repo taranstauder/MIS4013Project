815b113e05de534f2786ef3129211981bce72468
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-05 16:36:39 -0600

    Update trailer.php

--- trailer.php
+++ trailer.php
@@ -28,7 +28,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-            <option value="<?php $row['m.movid'];?>">
+            <option value="one">
               <div><?php echo $row['title']; ?></div>
           </option>
     <?php
