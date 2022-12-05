8912fce95a92113a2e672cdc6d26824c387f4760
Dasha Davydova <dasha.davydova@ou.edu>
2022-12-05 14:12:32 -0600

    Update admoviecopy.php

--- admoviecopy.php
+++ admoviecopy.php
@@ -77,7 +77,7 @@ if ($result->num_rows > 0) {
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="editMovieLabel">Edit a Movie</h1>
+                      <h1 class="modal-title fs-5" style="color:black" id="editMovieLabel">Edit a Movie</h1>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
@@ -133,7 +133,7 @@ $conn->close();
 
       <!-- Modal -->
       <div class="modal fade" id="addMovie" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addMovieLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog" style="color:black" >
           <div class="modal-content">
             <div class="modal-header">
               <h1 class="modal-title fs-5" id="addMovieLabel">Add a Movie</h1>
