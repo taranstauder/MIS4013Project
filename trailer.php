dc3b61f35467002bfaeb4303328bfd3aa0bef57d
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-05 13:44:46 -0600

    Update trailer.php

--- trailer.php
+++ trailer.php
@@ -2,8 +2,8 @@
 <html style="background: black;">
   <title>Reviews</title>
 <head> <?php include("header.php");?></head>
-<body style="background-color: black;">
-  <h3>Blake use this page to do the GET thing</h3>
+<body style="background-color: black; color: white;">
+  <h3>Blake use this page to do your part and to do the GET thing</h3>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
   <?php include("footer.php");?>
