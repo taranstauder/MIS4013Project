1b649b1343079dc72af7b84d8284ef7d93c8e18c
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-05 17:10:43 -0600

    Update displayTrailer.php

--- displayTrailer.php
+++ displayTrailer.php
@@ -1,4 +1,20 @@
 <?php
 $mName = $_GET["movieTrailer"];
-echo "See $mName";
+switch ($mName) {
+    case "Avatar":
+        break;
+    case "Elf":
+        break;
+    case "Harry Potter and the Sorcerer's Stone":
+        break;
+    case "Smile":
+        break;
+    case "Titanic":
+        break;
+    case "Top Gun: Maverick":
+        break;
+    case "Wall E":
+        echo "ur watching wall e";
+        break;
+}
 ?>
