dec39a5468a3a6e7899dbcd86fbc46b3ac1c5d54
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-05 18:25:31 -0600

    Update trailer.php

--- trailer.php
+++ trailer.php
@@ -5,7 +5,8 @@
     </title>
     <?php include("header.php");?>
 </head>
-<body>
+<body style="background-color:black;">
+  <div style="text-align:center;>
     <form action="displayTrailer.php" id="trailerForm" method="get">
         <select name="movieTrailer" id="movieTrailer" form="trailerForm">
           <option value="none" style="text-align: center;">Select a Movie</option>
@@ -43,6 +44,7 @@ $conn->close();
                   </select>
         <input type="submit" value="Watch the trailer!">
     </form>
+                                                       </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <?php include("footer.php");?>
 </body>
