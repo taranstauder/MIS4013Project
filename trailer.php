66cc3832e910f7e4e15436b0b7ef95cc013bf13a
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-05 22:10:16 -0600

    Update trailer.php

--- trailer.php
+++ trailer.php
@@ -43,7 +43,7 @@ $conn->close();
                                                         
 
              </select>
-   <input type="submit" value="Watch the trailer!">                                              
+   <input type="submit" value="Watch the trailer!" style="background:black; color:white;">                                              
     </form>          
   <br>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
