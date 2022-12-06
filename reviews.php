fa574ef61a8561a27f27840abe9c5d6243931f90
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-05 18:03:07 -0600

    Update reviews.php

--- reviews.php
+++ reviews.php
@@ -86,7 +86,7 @@ if ($result->num_rows > 0) {
       </button>
      
       <!-- Modal -->
-      <div class="modal fade" id="addOrders" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addOrdersLabel" aria-hidden="true" style="text-align:center;">
+      <div class="modal fade" id="addOrders" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addOrdersLabel" aria-hidden="true" style="align-content:center;">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
