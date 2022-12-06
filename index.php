8e7db34fc0a9162f051f40256dd50c61820507d9
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-06 11:33:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -30,13 +30,13 @@
     <div class="swiper mySwiper">
       <div class="swiper-wrapper">
         <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=5PSNL1qE6VY" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_FMjpg_UX1000_.jpg"></a></div>
-        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=BcDK7lkzzsU" target="_blank"><img src="https://i.ebayimg.com/thumbs/images/g/AkMAAOSwjjhi8Hn5/s-l1600.jpg"></a></div>
-        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=giXco2jaZ_4" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BMjNiMDA0OWQtZGY1MC00N2EwLTkxNDYtYmVkNTk1NzJiZjFmXkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg"></a></div>
+        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=BcDK7lkzzsU" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BZjE2ZWIwMWEtNGFlMy00ZjYzLWEzOWEtYzQ0MDAwZDRhYzNjXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UX1000_.jpg"></a></div>
+        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=giXco2jaZ_4" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg"></a></div>
         <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=FC6biTjEyZw" target="_blank"><img src="https://static.wixstatic.com/media/f86f23_b88ab82f5782484181d473d0ae304dd5~mv2.jpg/v1/fill/w_408,h_614,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/the-notebook1.jpg"></a></div>
-        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=gBKx63Y6rG4" target="_blank"><img src="https://c8.alamy.com/comp/B3KR2H/star-wars-poster-for-the-1977-tcflucasfilm-film-B3KR2H.jpg"></a></div>
-        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=kVrqfYjkTdQ" target="_blank"><img src="https://c8.alamy.com/comp/PXNB80/titanic-original-movie-poster-PXNB80.jpg"></a></div>
-        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=a54yC1etmVc" target="_blank"><img src="https://c8.alamy.com/comp/E5MCMW/elf-will-ferrell-2003-c-new-linecourtesy-everett-collection-E5MCMW.jpg"></a></div>
-        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=CZ1CATNbXg0" target="_blank"><img src="https://m.media-amazon.com/images/I/51iA8isg59L._AC_SY580_.jpg"></a></div>
+        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=gBKx63Y6rG4" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_.jpg"></a></div>
+        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=kVrqfYjkTdQ" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"></a></div>
+        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=a54yC1etmVc" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BMzUxNzkzMzQtYjIxZC00NzU0LThkYTQtZjNhNTljMTA1MDA1L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"></a></div>
+        <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=CZ1CATNbXg0" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_.jpg"></a></div>
         <div class="swiper-slide"><a href="https://www.youtube.com/watch?v=VyHV0BRtdxo" target="_blank"><img src="https://m.media-amazon.com/images/M/MV5BNmQ0ODBhMjUtNDRhOC00MGQzLTk5MTAtZDliODg5NmU5MjZhXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_.jpg"></a></div>
       </div>
       <div class="swiper-button-next"></div>
