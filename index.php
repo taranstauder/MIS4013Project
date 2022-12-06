2e7d6476892c1a27843559b23ddb6d328596f8f2
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-05 19:42:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,13 @@
     </style>
 </head>
 <body style="background-color: black;">
+  <link rel="stylesheet" href="css/audioplayer.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous">
+</script>
+<script src="js/audioplayer.js"></script>
+
 
 
     <div class="swiper mySwiper">
