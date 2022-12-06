60b25feb9399196daddc310ee7e13adf4df1e2b7
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-12-05 21:22:27 -0600

    Update trailer.php

--- trailer.php
+++ trailer.php
@@ -45,9 +45,7 @@ $conn->close();
              </select>                                         
     </form>   
   <br>
-  <div style="text-align:center; font-family: helvetica;">
   <input type="submit" value="Watch the trailer!">  
-  </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <?php include("footer.php");?>
 </body>
